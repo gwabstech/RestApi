181c92816cf8c078f2638275e3cca6f1f92487e7
Abubakar Abdullahi Gwabare <gwabstech@gmail.com>
2022-08-01 15:24:48 +0100

    Starting point

---
+++ src/main/java/com/gwabstech/RestApi/helloWorldController.java
@@ -0,0 +1,28 @@
+package com.gwabstech.RestApi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class helloWorldController {
+
+    //Get
+    // uri - /hello-
+    @GetMapping( path = "/hello-world")
+    public String helloWorld(){
+        return "Hello Abubakar";
+    }
+
+    @GetMapping("/hello-world-bean")
+    public HelloWorldBean helloWorldBean (){
+        return new HelloWorldBean("Hello Abubakar");
+    }
+
+    //adding a perimeter
+    @GetMapping("/hello-world-bean/{name}")
+    public HelloWorldBean helloWorldBean_p (@PathVariable String name){
+        return new HelloWorldBean(String.format("Hello, %s",name));
+    }
+
+}
